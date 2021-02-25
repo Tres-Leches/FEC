60d671801a9723cfeb92355c481241abcc6471bd
Chris Wang <heyitschrisw@gmail.com>
2021-02-25 09:51:25 -0800

    Completed setup for App.jsx

---
+++ client/src/components/Reviews/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from Reviews
+      </div>
+    );
+  }
+}
+
+export default Main;
