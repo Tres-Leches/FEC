38252b4b3e8434bfbc9fdf5b3e9af2a3a93373e7
Chris Wang <heyitschrisw@gmail.com>
2021-03-01 21:26:33 -0800

    Added helpful voting functionality to Questions

--- client/src/components/Questions Answers/QuestionsList.jsx
+++ client/src/components/Questions Answers/QuestionsList.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Question from './Question';
+import './questions.css';
 
 class QuestionsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isExpand: false,
-      hasMore: true,
     };
     this.toggleExpand = this.toggleExpand.bind(this);
   }
@@ -18,28 +18,36 @@ class QuestionsList extends React.Component {
   }
 
   render() {
-    const { questions } = this.props;
-    const { isExpand, hasMore } = this.state;
+    const { questions, getQuestions } = this.props;
+    const { isExpand } = this.state;
     const initialQ = questions.slice(0, 2);
     if (!isExpand) {
       return (
         <div>
-          {initialQ.map((question) => (
-            <Question key={question.question_id} question={question} />
-          ))}
-          {!hasMore && (
-          <button type="button" onClick={this.toggleExpand}>
-            More Answered Questions
-          </button>
-          )}
+          <div className="scroll">
+            {initialQ.map((question) => (
+              <Question
+                key={question.question_id}
+                question={question}
+                getQuestions={getQuestions}
+              />
+            ))}
+          </div>
+          <button type="button" onClick={this.toggleExpand}>More Answered Questions</button>
         </div>
       );
     }
     return (
       <div>
-        {questions.map((question) => (
-          <Question key={question.question_id} question={question} />
-        ))}
+        <div className="scroll">
+          {questions.map((question) => (
+            <Question
+              key={question.question_id}
+              question={question}
+              getQuestions={getQuestions}
+            />
+          ))}
+        </div>
         <button type="button" onClick={this.toggleExpand}>Less Questions</button>
       </div>
     );
