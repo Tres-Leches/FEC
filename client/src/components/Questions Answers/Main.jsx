d41f6e2e3fda4cc1331d5ec675d18d6731092ee6
Chris Wang <heyitschrisw@gmail.com>
2021-03-04 16:40:02 -0800

    Refactored questions button to only add two at a time

--- client/src/components/Questions Answers/Main.jsx
+++ client/src/components/Questions Answers/Main.jsx
@@ -74,24 +74,26 @@ class Main extends React.Component {
 
   render() {
     const { filteredQuestions, searchQuery, productId } = this.state;
-
-    return (
-      <div className="qa-main">
-        <div className="qa-main-header">
-          QUESTIONS AND ANSWERS
+    if (filteredQuestions.length) {
+      return (
+        <div className="qa-main">
+          <div className="qa-main-header">
+            QUESTIONS AND ANSWERS
+          </div>
+          <SearchBar
+            searchQuery={searchQuery}
+            setSearchQuery={this.setSearchQuery}
+            handleSearchSubmit={this.handleSearchSubmit}
+          />
+          <QuestionsList
+            questions={filteredQuestions}
+            getQuestions={this.getQuestions}
+            productId={productId}
+          />
         </div>
-        <SearchBar
-          searchQuery={searchQuery}
-          setSearchQuery={this.setSearchQuery}
-          handleSearchSubmit={this.handleSearchSubmit}
-        />
-        <QuestionsList
-          questions={filteredQuestions}
-          getQuestions={this.getQuestions}
-          productId={productId}
-        />
-      </div>
-    );
+      );
+    }
+    return null;
   }
 }
 
