f34618a8513399d202de22a158276a62881fe7f2
Chris Wang <heyitschrisw@gmail.com>
2021-03-10 11:40:08 -0800

    WIP Card component to display in carousel

--- client/src/components/Related Items/ProductCard.jsx
+++ client/src/components/Related Items/ProductCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
-const ProductCard = (props) => (
-  <div>
-    Card
-  </div>
-);
+const styles = {
+  height: 550,
+  width: 100,
+  objectFit: 'cover',
+};
+
+const ProductCard = (props) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={props.product.results[0].photos[0].thumbnail_url} style={styles} />
+      <Card.Body>
+        <Card.Title>CATEGORY</Card.Title>
+        <Card.Text></Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default ProductCard;
